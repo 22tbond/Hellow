de378cdaf72b91c028ac708e6078e1b3ee38198e
22tbond <44676528+22tbond@users.noreply.github.com>
2019-10-14 11:31:56 -0700

    Button light text

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
+brick.buttonEnter.onEvent(ButtonEvent.Pressed, function () {
+    brick.setStatusLight(StatusLight.Orange)
+    brick.showString("Hello world", 1)
+    brick.showString("Everything is awesome", 2)
+})
+forever(function () {
+
+})
 
